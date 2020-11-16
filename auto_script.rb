a80a1c2810a9e5eb30abb849e73b8e7283558672
Sarah Weaver <lwflouisa@gmail.com>
2020-11-16 00:07:28 -0500

    Update auto_script.rb

--- auto_script.rb
+++ auto_script.rb
@@ -1,2 +1,34 @@
-system("cd ai_scripter; ruby .scripter_seed.rb")
-system("cd ml_scripter; ruby .scripter_seed.rb")
+def script_rules
+  system("cd ai_scripter; ruby .scripter_seed.rb")
+  
+  puts "New self referenced method is scripted for rule set..."
+end
+  
+def script_ml
+  system("cd ml_scripter; ruby .scripter_seed.rb")
+  
+  puts "New self referenced method is scripted for machine learning..."
+end
+
+require "finite_machine"
+
+ai = FiniteMachine.new do
+  event :init,     :none    => :ruleset
+  event :rules,    :ruleset => :baysian
+  event :classify, :baysian => :none
+  
+  on_enter(:rules)    { |event| script_rules }
+  on_enter(:classify) { |event| script_ml    }
+end
+
+ai.init
+
+puts "The current process is #{ai.current}"
+
+ai.rules
+
+puts "The current process is #{ai.current}"
+
+ai.classify
+
+puts "The current process is #{ai.current}"
